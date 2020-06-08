725f31e47c8c4fe8c942078c03aa2302f6e70448
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-06-08 14:37:38 -0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,99 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { loadTasks, createNewTask, changeTaskStatus, deleteTask} from './reducers/taskReducer';
+import styled from 'styled-components'
+
 import './App.css'
+
+const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const Wrapper = styled.div`
+    width: 100%;
+    max-width: 30rem;
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const Form = styled.form`
+    width: 100%;
+    padding: 20px;
+    input {
+        width: 100%;
+    }
+`
+const TaskList = styled.ul`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding-left: 0px;
+`
+const Task = styled.li`
+    list-style-type: none;
+    label{
+        width: 100%;
+        margin: 0px;
+    }
+    span{
+        overflow-wrap: anywhere;
+    }
+    .task-body{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        cursor: pointer;
+        background-color: #f1f1f1;
+        padding: 1rem;
+        border-radius: 5px;
+    }
+    input[type='checkbox']:checked+.task-body{
+        background-color: #dedede;
+        color: #aaaaaa;
+        span{
+            text-decoration: line-through;
+        }
+    }
+
+    input[type='checkbox']:focus+.task-body{
+        box-shadow: 0 0 0 2pt red;
+    }
+    &+*{
+        margin-top: 1rem;
+
+        &:before {
+            content: '';
+            border-top: 1px solid #dddddd;
+            margin-left: 1rem;
+            margin-right: 1rem;
+            display: block;
+            margin-bottom: 1rem;
+        }
+    }
+    input{
+        opacity: 0;
+        position: absolute;
+        height: 0px;
+        width: 0px;
+    }
+`
+const Delete = styled.button`
+    border-radius: 50%;
+    width: 2rem;
+    height: 2rem;
+    font-weight: bold;
+    margin-left: 1rem;
+    user-select: none;
+    flex-shrink: 0;
+    &:focus{
+        outline: none;
+        box-shadow: 0 0 0 2pt red;
+    }
+`
 
 const App = () => {
     const [taskName, setTaskName] = useState('');
@@ -15,33 +107,39 @@ const App = () => {
 
     if(!tasks) return "Loading...";
     return (
-        <div className="App">
-            <form action="" onSubmit={e=>{
-                dispatch(createNewTask(taskName));
-                setTaskName('');
-                e.preventDefault();
-            }}>
-                <input
-                    value={taskName}
-                    placeholder="First Task"
-                    onChange={e => setTaskName(e.target.value)}
-                />
-                <button>Add</button>
-            </form>
-            <ul>
-                {tasks.map(({id, done, name}) => (
-                    <li key={id}>
-                        <input
-                            type="checkbox"
-                            checked={done}
-                            onChange={() => dispatch(changeTaskStatus(id, !done))}
-                        />
-                        {name} ({done ? 'Hecha' : 'Por hacer'})
-                        <button onClick={() => dispatch(deleteTask(id))}>Borrar</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <Main>
+            <Wrapper>
+                <Form action="" onSubmit={e=>{
+                    dispatch(createNewTask(taskName));
+                    setTaskName('');
+                    e.preventDefault();
+                }}>
+                    <input
+                        value={taskName}
+                        placeholder="Add task"
+                        onChange={e => setTaskName(e.target.value)}
+                    />
+                </Form>
+                <TaskList>
+                    {tasks.map(({id, done, name}) => (
+                        <Task key={id}>
+                            <label>
+                                <input
+                                    tabIndex={0}
+                                    type="checkbox"
+                                    checked={done}
+                                    onChange={() => dispatch(changeTaskStatus(id, !done))}
+                                />
+                                <div className="task-body">
+                                    <span>{name}</span>
+                                    <Delete onClick={() => dispatch(deleteTask(id))}>x</Delete>
+                                </div>
+                            </label>
+                        </Task>
+                    ))}
+                </TaskList>
+            </Wrapper>
+        </Main>
     )
 }
 
